20309b12670af2d3e87e0175df992d89382ac94b
akur15 <adikurniawanhmzh@gmail.com>
2024-12-08 20:21:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const displayHistory = document.querySelector{".display-history"};
-const display = document.querySelector{".display-input"};
-const tempResult = document.querySelector{".temp-result"};
+const displayHistory = document.querySelector(".display-history");
+const display = document.querySelector(".display-input");
+const tempResult = document.querySelector(".temp-result");
+const numbers = document.querySelectorAll(".number");
+const operation = document.querySelectorAll(".operation");
+const equal = document.querySelector(".equal");
+const clearAll = document.querySelector(".clear-all");
+const clearLast = document.querySelector(".last-entity-clear");
+
+let dis1Num = "";
+let dis2Num = "";
+let result = null;
+let lastOperation = "";
+let haveDot = false;
