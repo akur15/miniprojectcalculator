f6d342edf8e68e0d0cd5a0117d86a007f5119a74
akur15 <adikurniawanhmzh@gmail.com>
2024-12-08 19:54:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-javascript
+const displayHistory = document.querySelector{".display-history"};
+const display = document.querySelector{".display-input"};
+const tempResult = document.querySelector{".temp-result"};
